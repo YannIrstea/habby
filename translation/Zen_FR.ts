28767fba64aac94c422100b462484d417bd84699
dianevg <diane.von-gunten@irstea.fr>
2016-05-09 16:08:51 +0200

    HEC-RAS and HEC_RAS 2D added

---
+++ translation/Zen_FR.ts
@@ -0,0 +1,191 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>CentralW</name>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="168"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="169"/>
+        <source>Hydrology</source>
+        <translation>Hydrologie</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="170"/>
+        <source>Substrate</source>
+        <translation>Substrat</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="171"/>
+        <source>Run the model</source>
+        <translation>Exécuter le modèle</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="172"/>
+        <source>Output</source>
+        <translation>Sorties</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="173"/>
+        <source>Biology Info</source>
+        <translation>Info biologiques</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="174"/>
+        <source>Stat. Model</source>
+        <translation>Modèle stat.</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="175"/>
+        <source>Other</source>
+        <translation>Autre</translation>
+    </message>
+</context>
+<context>
+    <name>HydroW</name>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="246"/>
+        <source>I am hydro</source>
+        <translation>je suis hydro</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="249"/>
+        <source>I am really</source>
+        <translation>Vraiment c&apos;est moi</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindows</name>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="83"/>
+        <source>&amp;Exit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="85"/>
+        <source>Exit application</source>
+        <translation>Quitter le programme</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="87"/>
+        <source>Open Project</source>
+        <translation>Ouvrir un Projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="89"/>
+        <source>Open an exisiting project</source>
+        <translation>Ouvre un projet exisitant</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="91"/>
+        <source>New Project</source>
+        <translation>Nouveau projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="93"/>
+        <source>Create a new project</source>
+        <translation>Créer un nouveau projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="101"/>
+        <source>&amp;English</source>
+        <translation>Anglais</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="102"/>
+        <source>click here for English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="104"/>
+        <source>&amp;French</source>
+        <translation>Français</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="105"/>
+        <source>click here for French</source>
+        <translation>Cliquez ici pour le français</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="122"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="110"/>
+        <source>Get help to use the programme</source>
+        <translation>Aide </translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="114"/>
+        <source>&amp;File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="119"/>
+        <source>Language</source>
+        <translation>Langue</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="132"/>
+        <source>HABBY- ZEN INTERFACE</source>
+        <translation>HABBY INTERFACE ZEN</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="95"/>
+        <source>Save Project</source>
+        <translation>Sauvez le projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="97"/>
+        <source>Save the project</source>
+        <translation>Sauver le projet</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomeW</name>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="198"/>
+        <source>Project Name: </source>
+        <translation>Nom du projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="200"/>
+        <source>Main Folder: </source>
+        <translation>Dossier Principal</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="202"/>
+        <source>Set Folder</source>
+        <translation>Définir le Dossier</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="204"/>
+        <source>Description: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="206"/>
+        <source>User Name: </source>
+        <translation>Nom de l&apos;Utilisateur</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="210"/>
+        <source>New Project</source>
+        <translation>Nouveau projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="212"/>
+        <source>Open Project</source>
+        <translation>Ouvrir un Projet</translation>
+    </message>
+    <message>
+        <location filename="Main_windows_zen_tab.py" line="214"/>
+        <source>Save Project</source>
+        <translation>Sauver le Projet</translation>
+    </message>
+</context>
+</TS>
