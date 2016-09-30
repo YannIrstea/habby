3a525a034f123ea01bcb61c1383b4bca47a0da3e
dianevg <diane.von-gunten@irstea.fr>
2016-09-30 13:37:49 +0200

    update to setup.py to create .exe

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ setup(console=['habby.py'], #  windows=['Hec_ras01.py']
       options={
                 "py2exe":{
                         "includes": ["PyQt5.QtCore", "PyQt5.QtGui", "sip",  "matplotlib.backends.backend_tkagg", \
-                                     "tkinter.filedialog", "h5py","h5py.defs", "h5py.utils", "h5py.h5ac", 'h5py._proxy']
+                                     "tkinter.filedialog", "h5py","h5py.defs", "h5py.utils", "h5py.h5ac", 'h5py._proxy' \
+                                     ,'scipy.linalg.cython_blas', 'scipy.linalg.cython_lapack', \
+                                     'scipy.special._ufuncs_cxx', 'scipy.sparse.csgraph._validation']
                 }
         })
 
