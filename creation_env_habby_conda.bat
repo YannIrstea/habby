16eaeccb74c6b28ef140cc2ee9b2660bd2db963c
ROYER quentin <quentin.royer@irstea.priv>
2020-02-20 18:28:06 +0100

    h5py installation pip replaced by conda (error DLL, when read substrate instead of telemac work)

--- creation_env_habby_conda.bat
+++ creation_env_habby_conda.bat
@@ -3,26 +3,32 @@ call conda config --add channels conda-forge
 call conda config --add channels anaconda
 call conda config --add channels ramonaoptics
 
-:: CONDA and PIP
-call conda create --prefix C:\habby_dev\env_virtuels\env_habby_dev python=3.6 --yes
-call conda activate C:\habby_dev\env_virtuels\env_habby_dev
-call conda install -c conda-forge numpy conda-forge::blas=*=openblas --yes
+:: CONDA and PIP (virtual env + packages installation)
+call conda create --prefix C:\\habby_dev\\env_virtuels\\env_habby_dev python=3.6 --yes
+::call conda create --name env_habby_dev python=3.6 --yes
+call conda activate C:\\habby_dev\\env_virtuels\\env_habby_dev
+::call conda activate env_habby_dev
+call conda install -c conda-forge numpy conda-forge::blas=*=openblas --yes 
+call conda install pyinstaller=3.6 --yes
 call conda install gdal=3.0.2 --yes
 call conda install pyqt=5.9.2 --yes
 call conda install triangle=20170429 --yes
+call conda install h5py=2.10.0 --yes
+::pip install h5py==2.10.0
 pip install appdirs==1.4.3
 pip install qdarkstyle==2.8
 pip install matplotlib==3.1.3
-pip install h5py==2.10.0
 pip install numpy-stl==2.10.1
 pip install lxml==4.5.0
 pip install scipy==1.4.1
 pip install mplcursors==0.3
 pip install pillow==7.0.0
-pip install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
 
 :::::::::::::::::::::::::::: RUN HABBY :::::::::::::::
 python habby.py
+
+:: EXPORT TO YML
+call conda env export > env_habby_dev.yml
 
 :: Get console open to see details
 @pause 
