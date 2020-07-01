7f0e6199a430dacb9b9119f2c652ad3d6d650834
QuentinRoyerDev <quentin.royer@irstea.fr>
2020-07-01 17:59:30 +0200

    hydrosignature gui started

---
+++ src_GUI/hs_GUI.py
@@ -0,0 +1,57 @@
+"""
+This file is part of the free software:
+ _   _   ___  ______________   __
+| | | | / _ \ | ___ \ ___ \ \ / /
+| |_| |/ /_\ \| |_/ / |_/ /\ V /
+|  _  ||  _  || ___ \ ___ \ \ /
+| | | || | | || |_/ / |_/ / | |
+\_| |_/\_| |_/\____/\____/  \_/
+
+Copyright (c) IRSTEA-EDF-AFB 2017-2018
+
+Licence CeCILL v2.1
+
+https://github.com/YannIrstea/habby
+
+"""
+import os
+from multiprocessing import Process, Value
+
+from PyQt5.QtCore import pyqtSignal, Qt, QAbstractTableModel, QRect, QPoint, QVariant
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import QPushButton, QLabel, QListWidget, QAbstractItemView, QSpacerItem, \
+    QComboBox, QMessageBox, QFrame, QHeaderView, QLineEdit, QGridLayout, QFileDialog, QStyleOptionTab, \
+    QVBoxLayout, QHBoxLayout, QGroupBox, QSizePolicy, QScrollArea, QTableView, QTabBar, QStylePainter, QStyle
+
+from src.tools_mod import QGroupBoxCollapsible
+from src.hydraulic_process_mod import MyProcessList
+from src import hdf5_mod
+from src import plot_mod
+from src import tools_mod
+from src.project_properties_mod import load_project_properties
+
+
+class HsTab(QScrollArea):
+    """
+    This class contains the tab with Graphic production biological information (the curves of preference).
+    """
+    send_log = pyqtSignal(str, name='send_log')
+    """
+    A PyQt signal to send the log.
+    """
+
+    def __init__(self, path_prj, name_prj):
+        super().__init__()
+        self.tab_name = "hs"
+        self.mystdout = None
+        self.path_prj = path_prj
+        self.name_prj = name_prj
+        self.msg2 = QMessageBox()
+        self.init_iu()
+
+    def init_iu(self):
+        # insist on white background color (for linux, mac)
+        self.setAutoFillBackground(True)
+
+
+
