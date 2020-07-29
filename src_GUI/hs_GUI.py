dfbabe638a100f60a92d6d6d3a056b5270f4cb11
QuentinRoyerDev <quentin.royer@irstea.fr>
2020-07-29 18:55:05 +0200

    HS GUI in progress

--- src_GUI/hs_GUI.py
+++ src_GUI/hs_GUI.py
@@ -18,11 +18,11 @@ import os
 from multiprocessing import Process, Value
 
 from PyQt5.QtCore import pyqtSignal, Qt, QAbstractTableModel, QRect, QPoint, QVariant
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtGui import QStandardItemModel, QPixmap
 from PyQt5.QtWidgets import QPushButton, QLabel, QListWidget, QAbstractItemView, QSpacerItem, \
     QComboBox, QMessageBox, QFrame, QHeaderView, QLineEdit, QGridLayout, QFileDialog, QStyleOptionTab, \
     QVBoxLayout, QHBoxLayout, QGroupBox, QSizePolicy, QScrollArea, QTableView, QTabBar, QStylePainter, QStyle, \
-    QCheckBox, QListWidgetItem
+    QCheckBox, QListWidgetItem, QRadioButton
 from src import hydrosignature
 
 from src.tools_mod import QGroupBoxCollapsible
@@ -65,19 +65,6 @@ class HsTab(QScrollArea):
         tools_frame.setFrameShape(QFrame.NoFrame)
         tools_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        # file_selection
-        self.file_selection_label = QLabel(self.tr("Select a 2D mesh file :"))
-        self.file_selection_listwidget = QListWidget()
-        file_computed_layout = QHBoxLayout()
-        file_computed_label = QLabel(self.tr("HS value computed ?"))
-        self.file_computed_checkbox = QCheckBox()
-        file_computed_layout.addWidget(file_computed_label)
-        file_computed_layout.addWidget(self.file_computed_checkbox)
-        file_selection_layout = QVBoxLayout()
-        file_selection_layout.addWidget(self.file_selection_label)
-        file_selection_layout.addWidget(self.file_selection_listwidget)
-        file_selection_layout.addLayout(file_computed_layout)
-
         # computing
         self.computing_group = ComputingGroup(self.path_prj, self.name_prj, self.send_log, self.tr("Computing"))
         self.computing_group.setChecked(True)
@@ -86,18 +73,74 @@ class HsTab(QScrollArea):
         self.visual_group = VisualGroup(self.path_prj, self.name_prj, self.send_log, self.tr("Visualisation"))
         self.visual_group.setChecked(True)
 
+        # visual
+        self.compare_group = CompareGroup(self.path_prj, self.name_prj, self.send_log, self.tr("Comparison"))
+        self.compare_group.setChecked(True)
+
         # vertical layout
         self.setWidget(tools_frame)
         global_layout = QVBoxLayout()
         global_layout.setAlignment(Qt.AlignTop)
         tools_frame.setLayout(global_layout)
-        global_layout.addLayout(file_selection_layout)
         global_layout.addWidget(self.computing_group)
         global_layout.addWidget(self.visual_group)
+        global_layout.addWidget(self.compare_group)
         global_layout.addStretch()
+
+
+class ComputingGroup(QGroupBoxCollapsible):
+    """
+    This class is a subclass of class QGroupBox.
+    """
+
+    def __init__(self, path_prj, name_prj, send_log, title):
+        super().__init__()
+        self.path_prj = path_prj
+        self.name_prj = name_prj
+        self.send_log = send_log
+        self.path_last_file_loaded = self.path_prj
+        self.setTitle(title)
+        self.init_ui()
 
         # refresh_filenames
         self.refresh_filenames()
+
+    def init_ui(self):
+        # file_selection
+        file_selection_label = QLabel(self.tr("Select a 2D mesh file :"))
+        self.file_selection_listwidget = QListWidget()
+        self.file_selection_listwidget.setSelectionMode(QAbstractItemView.SingleSelection)
+        file_computed_label = QLabel(self.tr("HS value computed ?"))
+        self.file_computed_checkbox = QCheckBox()
+        input_class_label = QLabel(self.tr("Input class (.txt)"))
+        self.input_class_filename = QLabel(self.tr("..."))
+        self.input_class_pushbutton = QPushButton(self.tr("Select file"))
+        hs_export_txt_label = QLabel(self.tr("Export results (.txt)"))
+        self.hs_export_txt_checkbox = QCheckBox()
+        hs_export_mesh_label = QLabel(self.tr("Export mesh results (.hyd or .hab)"))
+        self.hs_export_mesh_checkbox = QCheckBox()
+        self.computation_pushbutton = QPushButton(self.tr("run"))
+
+        grid_layout = QGridLayout()
+
+        grid_layout.addWidget(file_selection_label, 0, 0)
+        grid_layout.addWidget(self.file_selection_listwidget, 1, 0)
+        grid_layout.addWidget(file_computed_label, 0, 1, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(self.file_computed_checkbox, 1, 1, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(input_class_label, 2, 0)
+        grid_layout.addWidget(self.input_class_filename, 2, 1, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(self.input_class_pushbutton, 2, 2)
+        grid_layout.addWidget(hs_export_txt_label, 3, 0)
+        grid_layout.addWidget(self.hs_export_txt_checkbox, 3, 1, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(hs_export_mesh_label, 4, 0)
+        grid_layout.addWidget(self.hs_export_mesh_checkbox, 4, 1, alignment=Qt.AlignCenter)
+        grid_layout.addWidget(self.computation_pushbutton, 5, 2)
+
+        grid_layout.setColumnStretch(0, 1)
+        grid_layout.setColumnStretch(1, 1)
+        grid_layout.setColumnStretch(2, 1)
+
+        self.setLayout(grid_layout)
 
     def refresh_filenames(self):
         # get list of file name by type
@@ -120,43 +163,6 @@ class HsTab(QScrollArea):
                 pass
 
 
-class ComputingGroup(QGroupBoxCollapsible):
-    """
-    This class is a subclass of class QGroupBox.
-    """
-
-    def __init__(self, path_prj, name_prj, send_log, title):
-        super().__init__()
-        self.path_prj = path_prj
-        self.name_prj = name_prj
-        self.send_log = send_log
-        self.path_last_file_loaded = self.path_prj
-        self.setTitle(title)
-        self.init_ui()
-
-    def init_ui(self):
-        input_class_label = QLabel(self.tr("Input class (.txt)"))
-        self.input_class_filename = QLabel(self.tr("..."))
-        self.input_class_pushbutton = QPushButton(self.tr("Select file"))
-        hs_export_txt_label = QLabel(self.tr("Export results (.txt)"))
-        self.hs_export_txt_checkbox = QCheckBox()
-        hs_export_mesh_label = QLabel(self.tr("Export mesh results (.hyd or .hab)"))
-        self.hs_export_mesh_checkbox = QCheckBox()
-        self.computation_pushbutton = QPushButton(self.tr("run"))
-
-        grid_layout = QGridLayout()
-        grid_layout.addWidget(input_class_label, 0, 0)
-        grid_layout.addWidget(self.input_class_filename, 0, 1)
-        grid_layout.addWidget(self.input_class_pushbutton, 0, 2)
-        grid_layout.addWidget(hs_export_txt_label, 1, 0)
-        grid_layout.addWidget(self.hs_export_txt_checkbox, 1, 2, alignment=Qt.AlignCenter)
-        grid_layout.addWidget(hs_export_mesh_label, 2, 0)
-        grid_layout.addWidget(self.hs_export_mesh_checkbox, 2, 2, alignment=Qt.AlignCenter)
-        grid_layout.addWidget(self.computation_pushbutton, 3, 2)
-
-        self.setLayout(grid_layout)
-
-
 class VisualGroup(QGroupBoxCollapsible):
     """
     This class is a subclass of class QGroupBox.
@@ -172,11 +178,76 @@ class VisualGroup(QGroupBoxCollapsible):
         self.init_ui()
 
     def init_ui(self):
-        self.input_pushbutton = QPushButton(self.tr("show input classes"))
-        self.output_pushbutton = QPushButton(self.tr("show output results"))
+
+        self.file_selection_listwidget = QListWidget()
+        self.reach_QListWidget = QListWidget()
+        self.units_QListWidget = QListWidget()
+
+
+        # axes mod
+
+
+        self.axe_mod_1_radio = QRadioButton()
+        self.axe_mod_1_radio.setChecked(True)  # TODO: save in json default and last choice (to be loaded)
+        self.axe_mod_1_radio.toggled.connect(self.change_axe_mod)
+        self.axe_mod_1_pixmap = QPixmap(r"translation/axe_mod_1.PNG").scaled(75, 75, Qt.KeepAspectRatio, Qt.SmoothTransformation)  # ,
+        axe_mod_1_label = QLabel()
+        axe_mod_1_label.setPixmap(self.axe_mod_1_pixmap)
+        self.axe_mod_2_radio = QRadioButton()
+        self.axe_mod_2_radio.toggled.connect(self.change_axe_mod)
+        self.axe_mod_2_pixmap = QPixmap(r"translation/axe_mod_2.PNG").scaled(75, 75, Qt.KeepAspectRatio, Qt.SmoothTransformation)  # ,
+        axe_mod_2_label = QLabel()
+        axe_mod_2_label.setPixmap(self.axe_mod_2_pixmap)
+        self.axe_mod_3_radio = QRadioButton()
+        self.axe_mod_3_radio.toggled.connect(self.change_axe_mod)
+        self.axe_mod_3_pixmap = QPixmap(r"translation/axe_mod_3.PNG").scaled(75, 75, Qt.KeepAspectRatio, Qt.SmoothTransformation)  # ,
+        axe_mod_3_label = QLabel()
+        axe_mod_3_label.setPixmap(self.axe_mod_3_pixmap)
+
+        axe_mod_layout = QHBoxLayout()
+        axe_mod_layout.addWidget(self.axe_mod_1_radio)
+        axe_mod_layout.addWidget(axe_mod_1_label)
+        axe_mod_layout.addWidget(self.axe_mod_2_radio)
+        axe_mod_layout.addWidget(axe_mod_2_label)
+        axe_mod_layout.addWidget(self.axe_mod_3_radio)
+        axe_mod_layout.addWidget(axe_mod_3_label)
+
+        selection_layout = QHBoxLayout()
+        selection_layout.addWidget(self.file_selection_listwidget)
+        selection_layout.addWidget(self.reach_QListWidget)
+        selection_layout.addWidget(self.units_QListWidget)
+        selection_layout.addLayout(axe_mod_layout)
+
+        general_layout = QVBoxLayout()
+        general_layout.addLayout(selection_layout)
+
+
+        self.setLayout(general_layout)
+
+    def change_axe_mod(self):
+        radio = self.sender()
+
+
+
+class CompareGroup(QGroupBoxCollapsible):
+    """
+    This class is a subclass of class QGroupBox.
+    """
+
+    def __init__(self, path_prj, name_prj, send_log, title):
+        super().__init__()
+        self.path_prj = path_prj
+        self.name_prj = name_prj
+        self.send_log = send_log
+        self.path_last_file_loaded = self.path_prj
+        self.setTitle(title)
+        self.init_ui()
+
+    def init_ui(self):
+
 
         grid_layout = QGridLayout()
-        grid_layout.addWidget(self.input_pushbutton, 0, 0)
-        grid_layout.addWidget(self.output_pushbutton, 1, 0)
+        # grid_layout.addWidget(self.input_pushbutton, 0, 0)
+        # grid_layout.addWidget(self.output_pushbutton, 1, 0)
 
         self.setLayout(grid_layout)
