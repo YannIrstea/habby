aae73ae4ce96b37000a0c7fc68c71e41ee5539f8
QuentinRoyerDev <quentin.royer@irstea.fr>
2020-09-03 10:45:20 +0200

    conda env creation bat improved

--- creation_env_habby_conda.bat
+++ creation_env_habby_conda.bat
@@ -21,28 +21,14 @@ if exist %envir_virtuels_path%\%envir_virtuel_name% rmdir /Q /S %envir_virtuels_
 call conda create --prefix %envir_virtuels_path%\%envir_virtuel_name% python=3.6 --yes
 call conda activate %envir_virtuels_path%\%envir_virtuel_name%
 call conda install -c conda-forge numpy conda-forge::blas=*=openblas --yes 
-call conda install pyinstaller=3.6 --yes
-::call conda install gdal=3.0.2 --yes
-call conda install pyqt=5.9.2 --yes
-::call conda install triangle=20170429 --yes
-call conda install h5py=2.10.0 --yes
-pip install "C:\habby_dev\dependence\GDAL-2.4.1-cp36-cp36m-win_amd64.whl"
-pip install git+git://github.com/drufat/triangle@master
-pip install appdirs==1.4.3
-pip install qdarkstyle==2.8
-pip install matplotlib==3.1.3
-pip install numpy-stl==2.10.1
-pip install lxml==4.5.0
-pip install scipy==1.4.1
-pip install mplcursors==0.3
-pip install pillow==7.0.0
-pip install pandas==1.0.3
+call conda install pyinstaller gdal --yes
+pip install PyQt5 h5py triangle appdirs qdarkstyle matplotlib numpy-stl lxml scipy mplcursors pillow pandas
 
 :::::::::::::::::::::::::::: RUN HABBY :::::::::::::::
 python %habby_path%\habby.py
 
 :: EXPORT TO YML
-conda env export > env_habby_dev.yml
+::conda env export > env_habby_dev.yml
 
 :: CREATE env_habby_dev VIRTUAL ENV CONDA FROM .yml
 ::call conda env create --file env_habby_dev.yml
