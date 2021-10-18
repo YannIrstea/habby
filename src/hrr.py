261b5d1e32ae6f565a7539a92aa3e28fce9c4d73
QuentinRoyerDev <quentin.royer@irstea.fr>
2021-10-18 15:36:01 +0200

    hrr starting 2

--- src/hrr.py
+++ src/hrr.py
@@ -7,13 +7,28 @@ if __name__ == '__main__':
 
     # first file
     input_filename_1 = "d1.hyd"
-    hdf5_1 = Hdf5Management(path_prj, input_filename_1, new=False, edit=False)
-    hdf5_1.load_hdf5()
-    hdf5_1.data_2d[0][0].c_mesh_height()  # compute h mesh from h node (memory)
 
-    # get tin first reach first unit
-    tin = hdf5_1.data_2d[0][0]["mesh"]["tin"]
-    xy = hdf5_1.data_2d[0][0]["node"]["xy"]
-    z_fond_node = hdf5_1.data_2d[0][0]["node"]["z"]
-    h_node = hdf5_1.data_2d[0][0]["node"]["data"]["h"]
-    h_mesh = hdf5_1.data_2d[0][0]["mesh"]["data"]["h"]
+    # load file
+    hdf5_1 = Hdf5Management(path_prj, input_filename_1, new=False, edit=False)
+    hdf5_1.load_hdf5(whole_profil=True)
+
+    # for all reach
+    for reach_number in range(0, hdf5_1.data_2d.reach_number):
+        # for all units
+        for unit_number in range(0, hdf5_1.data_2d.unit_number):
+            # c_mesh_height
+            hdf5_1.data_2d[reach_number][unit_number].c_mesh_height()  # compute h mesh from h node (memory)
+
+            # get tin first reach first unit
+            tin = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["tin"]
+            xy = hdf5_1.data_2d[reach_number][unit_number]["node"]["xy"]
+            z_fond_node = hdf5_1.data_2d[reach_number][unit_number]["node"]["z"]
+            h_node = hdf5_1.data_2d[reach_number][unit_number]["node"]["data"]["h"]
+            h_mesh = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["data"]["h"]
+            i_whole_profile = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["i_whole_profile"]
+            i_split = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["data"]["i_split"]
+
+            # get tin first reach first unit whole_profile
+            xy_whole_profile = hdf5_1.data_2d_whole[reach_number][unit_number]["node"]["xy"]
+            tin_whole_profile = hdf5_1.data_2d_whole[reach_number][unit_number]["mesh"]["tin"]
+
