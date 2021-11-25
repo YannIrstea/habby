0a54fa30ddd7f8ab7286b3146bb44c51b634b385
QuentinRoyerDev <quentin.royer@irstea.fr>
2021-11-25 11:28:35 +0100

    v1.5.0

--- habby.py
+++ habby.py
@@ -25,7 +25,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QSplashScreen, QGraphicsOpacityEffect
 from appdirs import AppDirs
 
-HABBY_VERSION_STR = "1.4.2"
+HABBY_VERSION_STR = "1.5.0"
 
 
 class AppDataFolders:
