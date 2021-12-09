2b517e956e530db1c8ebce41dcd16a3f4066cb28
QuentinRoyerDev <quentin.royer@inrae.fr>
2021-12-09 22:47:34 +0100

    zip setup file added to automatic executable file creation

--- tips/executables/habby_to_exe_to_setup_pyinstaller.cmd
+++ tips/executables/habby_to_exe_to_setup_pyinstaller.cmd
@@ -27,7 +27,7 @@ ECHO pyinstaller ##  --windowed remove console  --specpath=pyinstaller_config.sp
 ::pyinstaller --icon=file_dep\habby_icon.ico --windowed --distpath=build\pyinstaller --workpath=build\pyinstaller\temp --name=habby habby.py
 pyinstaller tips\executables\habby.spec --distpath=build\pyinstaller --workpath=build\pyinstaller\temp --noconfirm
 
-ECHO if build folder exist, remove it
+ECHO remove temp folder
 if exist build\pyinstaller\temp rmdir /Q /S build\pyinstaller\temp
 if exist __pycache__ rmdir /Q /S __pycache__
 
@@ -39,15 +39,15 @@ robocopy translation build\pyinstaller\habby\translation /E > nul
 robocopy file_dep build\pyinstaller\habby\file_dep /E > nul
 
 if %VarQuestion%== n ECHO run executable to see errors
-if %VarQuestion%== n cd build\pyinstaller\habby\
-if %VarQuestion%== n habby.exe
-if %VarQuestion%== n cd ..\..\..
+if %VarQuestion%== n build\pyinstaller\habby\habby.exe
 if %VarQuestion%== n ECHO executable created in ..\habby\build\pyinstaller
 
 if %VarQuestion%== n ECHO Executable successfully created (not setup)
 if %VarQuestion%== y ECHO Executable successfully created and setup file creating..
 if %VarQuestion%== y start "" /w "C:\Program Files (x86)\Inno Script Studio\isstudio.exe" -compile tips\executables\setup_from_pyinstaller.iss
-if %VarQuestion%== y ECHO Setup successfully created
+:: .exe to zip with tar.exe (-C: directory, -a: gzip mode, -c: create, -f: file, full path to ouptput file, filename to zip in specified directory)
+if %VarQuestion%== y tar.exe -C build\pyinstaller -a -c -f build\pyinstaller\HABBY-setup-64.zip HABBY-setup-64.exe
+if %VarQuestion%== y ECHO Setup file and archive successfully created
 
 :: Get console open to see details, press enter
 @pause 
