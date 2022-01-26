5e5c286f86c415e2d90fa105f3b14f576f5892a5
QuentinRoyerDev <quentin.royer@inrae.fr>
2022-01-26 16:53:01 +0100

    mesh manager corrections

--- src/mesh_manager_mod.py
+++ src/mesh_manager_mod.py
@@ -150,12 +150,19 @@ def mesh_manager(mesh_manager_description, progress_value, q=[], print_cmd=False
             else:
                 cell_index = mesh_manager_description["mesh_manager_data"]
 
+            same_len = len(hdf5_1.data_2d[reach_number][unit_number]["mesh"]["tin"]) == len(cell_index)
+
             # change data
             if eliminate:  # eliminate
+                if same_len:
+                    print("Warning: All cell of unit " + hdf5_1.data_2d[reach_number][unit_number].unit_name + " are removed.")
+
                 hdf5_1.data_2d[reach_number][unit_number]["mesh"]["tin"] = np.delete(hdf5_1.data_2d[reach_number][unit_number]["mesh"]["tin"], cell_index, 0)
                 hdf5_1.data_2d[reach_number][unit_number]["mesh"]["i_whole_profile"] = np.delete(hdf5_1.data_2d[reach_number][unit_number]["mesh"]["i_whole_profile"], cell_index, 0)
                 hdf5_1.data_2d[reach_number][unit_number]["mesh"]["data"] = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["data"].drop(cell_index)
             else:  # keep
+                if same_len:
+                    print("Warning: All selected cell of unit " + hdf5_1.data_2d[reach_number][unit_number].unit_name + " are keep. Nothing happen.")
                 hdf5_1.data_2d[reach_number][unit_number]["mesh"]["tin"] = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["tin"][cell_index]
                 hdf5_1.data_2d[reach_number][unit_number]["mesh"]["i_whole_profile"] = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["i_whole_profile"][cell_index]
                 hdf5_1.data_2d[reach_number][unit_number]["mesh"]["data"] = hdf5_1.data_2d[reach_number][unit_number]["mesh"]["data"].iloc[cell_index]
@@ -166,17 +173,38 @@ def mesh_manager(mesh_manager_description, progress_value, q=[], print_cmd=False
             # progress
             progress_value.value = progress_value.value + delta_unit
 
-    # hvum copy
-    hdf5_1.data_2d.filename = hdf5_1.filename[:-4] + "_MM" + hdf5_1.extension
+    new_filename = hdf5_1.filename[:-4] + "_MM" + hdf5_1.extension
 
     # get_dimension
     hdf5_1.data_2d.get_dimension()
     # export new hdf5
     hdf5 = Hdf5Management(project_properties["path_prj"],
-                          hdf5_1.data_2d.filename, new=True)
-    hdf5.create_hdf5_hyd(hdf5_1.data_2d,
-                         hdf5_1.data_2d_whole,
-                         project_properties)
+                          new_filename, new=True)
+    if hdf5_1.extension == ".hyd":
+        hdf5.create_hdf5_hyd(hdf5_1.data_2d,
+                             hdf5_1.data_2d_whole,
+                             project_properties)
+    else:
+
+        hdf5.create_hdf5_hab(hdf5_1.data_2d,
+                             hdf5_1.data_2d_whole,
+                             project_properties)
+    # load original hydrosignature
+    hdf5_original = Hdf5Management(project_properties["path_prj"],
+                                hdf5_1.data_2d.filename, new=False, edit=False)
+    hdf5_original.load_hydrosignature()
+    # TODO: change new file hydrosignature
+    # set to new file
+    hdf5 = Hdf5Management(project_properties["path_prj"],
+                          hdf5.filename, new=False, edit=True)
+    hdf5.get_hdf5_attributes(close_file=False)
+    hdf5.load_units_index()
+    hdf5.load_data_2d()
+    hdf5.load_whole_profile()
+    hdf5.data_2d = hdf5_original.data_2d
+    hdf5.write_hydrosignature(hs_export_mesh=hdf5_original.hs_mesh)
+    hdf5.close_file()
+
 
     # warnings
     if not print_cmd:
