d6320819f681d3e52f6f5be605b4c67a5d2452f8
YannIrstea <yann.lecoarer@irstea.fr>
2023-08-02 18:10:55 +0200

    updating .sh to create vitual environement for linux Ubuntu 22.04.2 and python 3.10

--- tips/virtual_envs/creation_env_habby_pip.sh
+++ tips/virtual_envs/creation_env_habby_pip.sh
@@ -4,17 +4,20 @@ pip3 install virtualenv
 
 cd /datas/habby_dev/env_virtuels/
 
-virtualenv --python /usr/bin/python3.6 env_habby_dev_pip
+virtualenv --python /usr/bin/python3.10 env_habby_dev_pip
 
 ## activate
 source /datas/habby_dev/env_virtuels/env_habby_dev_pip/bin/activate
 
 pip3 install pip --upgrade
+## install first gdal linux and then gdal python
+## sudo apt-get install gdal-bin
+## sudo apt-get install libgdal-dev
+## and for pip3 specify the good version of gdal
+pip3 install gdal==3.4.1 --global-option=build_ext --global-option="-I/usr/include/gdal/"
 
-pip3 install gdal==2.2.3 --global-option=build_ext --global-option="-I/usr/include/gdal/"
-
-pip3 install -r $HOME/habby_dev/habby/requirements.txt
+pip3 install -r $HOME/habby/requirements.txt
 
 ## RUN HABBY
-python $HOME/habby_dev/habby/habby.py
+python $HOME/habby/habby.py
 
