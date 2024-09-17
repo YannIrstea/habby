80c2a31c296a282262816ffa80091b51947d7e47
Yann Le-Coarer <yann.lecoarer@inrae.fr>
2024-09-17 15:14:09 +0200

    version number 1.6.3

--- habby.py
+++ habby.py
@@ -24,7 +24,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QSplashScreen, QGraphicsOpacityEffect
 from appdirs import AppDirs
 
-HABBY_VERSION_STR = "1.6.2"
+HABBY_VERSION_STR = "1.6.3"
 
 
 class AppDataFolders:
