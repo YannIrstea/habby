4790060921432c11df436a0555317cd0bdf10de0
QuentinRoyerDev <quentin.royer06@hotmail.fr>
2026-06-04 15:43:33 +0200

    version changed to 1.6.14

--- habby.py
+++ habby.py
@@ -24,7 +24,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QSplashScreen, QGraphicsOpacityEffect
 from appdirs import AppDirs
 
-HABBY_VERSION_STR = "1.6.13"
+HABBY_VERSION_STR = "1.6.14"
 
 
 class AppDataFolders:
